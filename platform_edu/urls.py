52c0fb3dee0486f06f48424ce7832e5917744f9b
razveetonormalno <mrdjon0506@gmail.com>
2023-05-30 18:44:24 +0300

    Add videochat

--- platform_edu/urls.py
+++ platform_edu/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, re_path
 from .views import *
+from . import views
 
 urlpatterns = [
     path('', index, name='index'),  # 127.0.0.1:8000
     path('cats/<int:catid>/', categories),  # 127.0.0.1:8000/cats/<int:catid>/
     re_path(r'^archive/(?P<year>[0-9]{4})', archive),
+    # path('accounts/signup/', SignUp.as_view(), name='registration')
+    path('home/videochat/', videochat, name="createroom")
 ]
--- platform_edu/views.py
+++ platform_edu/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+# from django.urls import reverse_lazy
+# from django.contrib.auth.forms import UserCreationForm
+# from django.views.generic.edit import CreateView
+# from .forms import CreationForm
 
 def home_page(request):
     return render(request, "main.html")
+
+def videochat(request):
+    video = """
+<div id="metered-frame"></div>
+<script src="https://cdn.metered.ca/sdk/frame/1.4.3/sdk-frame.min.js"></script>
+<script>
+    var frame = new MeteredFrame(); 
+    frame.init({
+        // This URL will be different. It will be unique based on your appName and roomName
+        roomURL: "yourappname.metered.live/tutorial", 
+    }, document.getElementById("metered-frame"));
+</script>
+"""
+    return HttpResponse(video)
+
+# def registration(request):
+#     return render(request, "registration/signup.html")
+
+# class SignUp(CreateView):
+#     form_class = CreationForm
+#     success_url = reverse_lazy("login")
+#     template_name = "registration/signup.html"
 
 def index(request):
     return HttpResponse("Страница приложения EasyEdu.")
