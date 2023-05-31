780fa97224b75fcbcd7d7461fe65c299c2ab19ff
razveetonormalno <mrdjon0506@gmail.com>
2023-05-31 18:19:36 +0300

    Add login, logout and check for authentication

--- EasyEdu/settings.py
+++ EasyEdu/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'EasyEdu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,4 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = "/home"
--- platform_edu/urls.py
+++ platform_edu/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from .views import *
-from . import views
+
 
 urlpatterns = [
     path('', index, name='index'),  # 127.0.0.1:8000
